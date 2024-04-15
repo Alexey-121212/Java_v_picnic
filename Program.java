f767f55eff73e0f7c80b728817867cda487ad08e
Alexey <alexmail121212@gmail.com>
2024-04-16 00:37:18 +0300

    done

--- Program.java
+++ Program.java
@@ -1,5 +1,13 @@
 public class Program {
     public static void main(String[] args) {
-
+        try {
+            VirtualPicnicBasket basket = new VirtualPicnicBasket("input.txt", " ");
+            System.out.println("Количество слов " + basket.getNumWords());
+            System.out.println("Самое длинное слово " + basket.getMostLongWord());
+            System.out.println("Количество использований слов: " + basket.countNumEachWord());
+        } catch (Exception e) {
+            System.out.println("Что-то пошло не так, что конкретно - ответ в Stacktrace");
+            e.printStackTrace();
+        }
     }
 }
---
+++ VirtualPicnicBasket.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class VirtualPicnicBasket {
+
+    private ArrayList<String> items = new ArrayList();
+
+    public VirtualPicnicBasket(String fileName, String delimeter) throws Exception {
+        Scanner scanner = new Scanner(new File(fileName));
+        scanner.useDelimiter(delimeter);
+        while (scanner.hasNext()) {
+            items.add(scanner.next().replaceAll("(\\r|\\n)", ""));
+        }
+        scanner.close();
+        if (getNumWords() == 0) throw new Exception("Не удалось загрузить данные");
+    }
+
+    public int getNumWords() {
+        return items.size();
+    }
+
+    public String getMostLongWord() {
+        int longestWordLen = 0;
+        String longestWord = "";
+
+        for (String word : items) {
+            if (longestWordLen < word.length()) {
+                longestWordLen = word.length();
+                longestWord = word;
+            }
+        }
+        return longestWord;
+    }
+
+    public HashMap<String, Integer> countNumEachWord() {
+        HashMap<String, Integer> hashMap = new HashMap();
+
+        for (String word : items) {
+            if (hashMap.containsKey(word))
+                hashMap.put(word, hashMap.get(word) + 1);
+            else
+                hashMap.put(word, 1);
+        }
+        return hashMap;
+    }
+}
